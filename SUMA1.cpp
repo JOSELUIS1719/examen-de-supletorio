833b7ac4f9b295c82add3f64fe8585554b42352b
JOSE ERAZO <101337417+JOSELUIS1719@users.noreply.github.com>
2022-06-07 09:33:06 -0500

    Add files via upload

---
+++ SUMA1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	float A1,B2,C3,D4;
+	cout<<"ingrese el valor de A1: "; cin>>A1;
+	cout<<"ingrese el valor de B2: "; cin>>B2;
+	cout<<"ingrese el valor de C3: "; cin>>C3;
+	D4=A1+B2+C3;
+	cout<<"El resultado de la suma es: ";
+	cout<<D4;
+	return 0;
+}
